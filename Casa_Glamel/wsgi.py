74cb7f22333dd45b1f40fac57ddd9cfcc5cc3a42
Lmontoyacontreras <Lmontoyacontreras@gmail.com>
2015-07-31 00:28:09 -0500

    commit 06

--- Casa_Glamel/wsgi.py
+++ Casa_Glamel/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Casa_Glamel.staging")
-application = get_wsgi_application()
-#application = Cling(get_wsgi_application())
+
+application = Cling(get_wsgi_application())
