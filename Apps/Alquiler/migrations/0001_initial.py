51dccd14d9efe558db5380f5fd2f99dda597844f
Lmontoyacontreras <Lmontoyacontreras@gmail.com>
2015-07-31 01:16:47 -0500

    commit 02

--- Apps/Alquiler/migrations/0001_initial.py
+++ Apps/Alquiler/migrations/0001_initial.py
@@ -7,19 +7,19 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Inventario', '0005_auto_20150724_1319'),
-        ('GestionInf', '0003_cliente'),
+        ('GestionInf', '0001_initial'),
+        ('Inventario', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Alquiler',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('vendedor', models.CharField(max_length=50)),
-                ('fecha_entrega', models.DateTimeField()),
-                ('fecha_devolucion', models.DateTimeField()),
-                ('devuelto', models.NullBooleanField()),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('vendedor', models.CharField(blank=True, max_length=50)),
+                ('fecha_entrega', models.DateField()),
+                ('fecha_devolucion', models.DateField()),
+                ('devuelto', models.BooleanField(default=False)),
                 ('deposito', models.IntegerField()),
                 ('observaciones', models.TextField()),
                 ('cliente', models.ForeignKey(to='GestionInf.Cliente')),
@@ -28,8 +28,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Alquiler_Detail',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
                 ('gratis', models.BooleanField(default=False)),
+                ('precio', models.IntegerField()),
                 ('alquiler', models.ForeignKey(to='Alquiler.Alquiler')),
                 ('articulo', models.ForeignKey(to='Inventario.Articulo')),
             ],
